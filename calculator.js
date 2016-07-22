e2b54b5bd436d3e193bdc59eb80f645aa279f8dc
pragyagoel04 <pragyagoel04@gmail.com>
2016-07-21 17:54:06 -0700

    added on-click button functions

--- calculator.js
+++ calculator.js
@@ -2,52 +2,23 @@
 var a=0;
 var b=0;
 var i=0;
+var next_operator='';
 //arithmatic functions
-"pragya"
 var add=function(a,b){
-    return a + b;
-}
+    return (a + b);
+};
 
 var sub=function(a,b){
-    return a - b;
-}
+    return (a - b);
+};
 
 var mul=function(a,b){
-    return a * b;
-}
+    return (a * b);
+};
 
 var div=function(a,b){
-    return a / b;
-}
-
-var mod=function(a,b){
-    return a % b;
-}
-
-var equals=function(){
-    if(input[1]=="+"){
-        clear();
-        value.push(add(parseFloat(input[0]), parseFloat(input[2])));
-    }
-    else if(input[1]=="-"){
-        clear();
-        value.push(sub(parseFloat(input[0]), parseFloat(input[2])));
-    }
-    else if(input[1]=="*"){
-        clear();
-        value.push(mul(parseFloat(input[0]), parseFloat(input[2])));
-    }
-    else if(input[1]=="/"){
-        clear();
-        value.push(div(parseFloat(input[0]), parseFloat(input[2])));
-    }
-    else if(input[1]=="%"){
-        clear();
-        value.push(mod(parseFloat(input[0]), parseFloat(input[2])));
-    }
-
-    display();
-}
+    return (a / b);
+};
 
 var press_button = function(current_button){
   if (a.length != 0 && i == 0) {
@@ -58,28 +29,39 @@ var press_button = function(current_button){
     i++;
     display(current_button);
   }
-}
+};
 
-var get_number=function{
-    if(a==0)
+
+var get_number=function(){
+    if(a==0){
         a=document.getElementById("screen").innerHTML;
+    }
     else{
         b=document.getElementById("screen").innerHTML;
     }
-}
+};
 
 
 //display functions
 
+
+var reset=function(){
+    document.getElementById("screen").innerHTML="";
+    a=0;
+    b=0;
+    i=0;
+    next_operator=0;    
+};
+
 val clear=function(){
     document.getElementById("screen").innerHTML="";
-}
+};
 
 var update=function(value){
     input.push(value);
     //shift() removes first element of the array;
     input.shift();
-}
+};
 
 
 var display=function(number){
@@ -88,12 +70,129 @@ var display=function(number){
         displayNum.innerHTML="";
         displayNum.innerHTML=displayNum.innerHTML + number;
     } 
-    else{
-
+    else if(i > 10) {
+        document.getElementById("screen").style.display = "none";
+        setTimeout(function(){ document.getElementById("screen").style.display = "block";}, 100)
+        document.getElementById("screen").innerHTML = "Too long";
     }
-}
-    //innerHtml changes the value present in result to new value
-    result.innerHTML=input.join(" ") + " " + value.join("");
-}
+    else{
+        displayNum.innerHTML=displayNum.innerHTML + number;
+    }
 
-//event listners 
+};
+
+var get_result = function() {
+  get_number();
+  if (next_operator === "add") {
+    clear_display();
+    i = (add(a, b)).length
+    display(add(a, b));
+  } 
+  else if (next_operator === "subtract") {
+    clear_display();
+    i = (subtract(a, b)).length
+    display(subtract(a, b));
+  } 
+  else if (next_operator === "multiply") {
+    clear_display();
+    i = (multiply(a, b)).length
+    display(multiply(a, b));
+  } 
+  else if (next_operator === "divide") {
+    clear_display();
+    i = (divide(a, b)).length
+    display(divide(a, b));
+  }
+};
+
+var calculate_step = function() {
+  if (a != 0 && b != 0) {
+    get_result();
+    a = document.getElementById("screen").innerHTML;
+    b = 0;
+    i = 0;
+  }
+};
+
+
+var operator = function(sign) {
+  if (sign === "equals") {
+    get_result();
+    a = 0
+    b = 0;
+    i = 0;
+  } else if (sign === "negate") {
+    var num = document.getElementById("screen").innerHTML;
+    document.getElementById("screen").innerHTML = +num * -1;
+  } else {
+    next_operator = sign;
+    get_number();
+    calculate_step();
+    i = 0;
+  }
+};
+
+document.onkeyup = function(e) {
+  e = e || event
+  if(!e.shiftKey){
+    switch(e.keyCode){
+      case 49:
+        press_button(1);
+        break;
+      case 50:
+        press_button(2);
+        break;
+      case 51:
+        press_button(3);
+        break;
+      case 52:
+        press_button(4);
+        break;
+      case 53:
+        press_button(5);
+        break;
+      case 54:
+        press_button(6);
+        break;
+      case 55:
+        press_button(7);
+        break;
+      case 56:
+        press_button(8);
+        break;
+      case 57:
+        press_button(9);
+        break;
+      case 48:
+        press_button(0);
+        break;
+      case 187:
+        operator('equals');
+        break;
+      case 189:
+        operator('subtract');
+        break;
+      case 88:
+        operator('multiply');
+        break;
+      case 191:
+        operator('divide');
+      case 190:
+        press_button('.');
+        break;
+      case 13:
+        operator('equals');
+        break;
+      case 27:
+        reset();
+    }
+  } else if(e.shiftKey) {
+    switch(e.keyCode){
+      case 187:
+        operator('add');
+        break;
+      case 56:
+        operator('multiply');
+    }
+  }
+};
